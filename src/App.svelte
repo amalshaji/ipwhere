9f3c0949fec381063fd9e73d107369d6b6716871
Amal Shaji <amalshaji@tutanota.com>
2020-10-11 13:27:56 +0530

    beautify frontend

--- src/App.svelte
+++ src/App.svelte
@@ -29,18 +29,35 @@
 </script>
 
 <style>
-	.container-fluid {
+	.container {
 		margin-top: 50px;
-		max-height: 100%;
+		min-height: 100%;
+		padding: 0;
 		margin-left: 10%;
 		max-width: 80%;
 	}
 	#ip-add {
 		max-width: 70%;
 	}
+
+	#map {
+		width: 100%;
+		height: 300px;
+		overflow: auto;
+	}
+	#footer {
+		height: 50px; /* the footer's total height */
+	}
+
+	#footer-content {
+		background-color: #f3e5f5;
+		border: 1px solid #ab47bc;
+		height: 32px; /* height + top/bottom paddding + top/bottom border must add up to footer height */
+		padding: 8px;
+	}
 </style>
 
-<div class="container-fluid">
+<div class="container">
 	<center>
 		<h2>IP Geolocator</h2>
 		<input type="text" bind:value={ip} id="ip-add" />
@@ -55,16 +72,18 @@
 				<b>{isp}</b>
 			</div><br /><br />
 			<iframe
+				id="map"
 				title="map"
-				width="100%"
-				height="200%"
 				frameborder="0"
 				scrolling="no"
 				marginheight="0"
 				marginwidth="0"
-				src="https://www.openstreetmap.org/export/embed.html?bbox={lon - 0.5}%2C{lat - 0.5}%2C{lon + 0.5}%2C{lat + 0.5}&layer=mapnik&marker={lat}%2C{lon}"
+				src="https://www.openstreetmap.org/export/embed.html?bbox={lon - 0.3}%2C{lat - 0.3}%2C{lon + 0.3}%2C{lat + 0.3}&layer=mapnik&marker={lat}%2C{lon}"
 				style="border: 1px solid black" /><br />
 		</div>
 		<div hidden id="error">{message}</div>
-	</center>
+	</center><br /><br /><br /><br />
+	<footer id="footer">
+		<div id="footer-content">Sticky Footer</div>
+	</footer>
 </div>
