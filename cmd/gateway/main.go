72e16591f98b2a3b81a1763d4cb3cf61e523c2c4
Amal Shaji <amalshaji@tutanota.com>
2020-10-11 11:41:39 +0530

    refactored code

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -2,64 +2,14 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
 	"time"
 
 	"github.com/amalshaji/fiber-netlify/adapter"
+	"github.com/amalshaji/fiber-netlify/handler"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cache"
 )
-
-// func main() {
-// 	port := flag.Int("port", -1, "specify a port to use http rather than AWS Lambda")
-// 	flag.Parse()
-// 	listener := gateway.ListenAndServe
-// 	portStr := ""
-// 	if *port != -1 {
-// 		portStr = fmt.Sprintf(":%d", *port)
-// 		listener = http.ListenAndServe
-// 		http.Handle("/", http.FileServer(http.Dir("./public")))
-// 	}
-
-// 	http.Handle("/api/feed", feed2json.Handler(
-// 		feed2json.StaticURLInjector("https://news.ycombinator.com/rss"),
-// 		nil, nil, nil, cacheControlMiddleware))
-// 	log.Fatal(listener(portStr, nil))
-// }
-type response struct {
-	Status    string  `json:"status"`
-	Country   string  `json:"country"`
-	Region    string  `json:"regionName"`
-	Latitude  float32 `json:"lat"`
-	Longitude float32 `json:"lon"`
-	ISP       string  `json:"isp"`
-}
-
-func cacheRequest(exp time.Duration) fiber.Handler {
-	return cache.New(cache.Config{
-		Expiration:   exp,
-		CacheControl: true,
-	})
-}
-
-func getGeoLocation(c *fiber.Ctx) error {
-	ip := c.Params("ip")
-	res, _ := http.Get("http://ip-api.com/json/" + ip)
-	body, _ := ioutil.ReadAll(res.Body)
-
-	var resp response
-	json.Unmarshal(body, &resp)
-	if resp.Status == "fail" {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"message": "enter an ip",
-		})
-	}
-	return c.Status(fiber.StatusOK).JSON(resp)
-}
 
 var fiberLambda *adapter.FiberLambda
 
@@ -71,16 +21,18 @@ func init() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendFile("index")
 	})
-	app.Get("/api/:ip", cacheRequest(10*time.Minute), getGeoLocation)
+	app.Get("/api/:ip", handler.CacheRequest(10*time.Minute), handler.GeoLocation)
 
 	fiberLambda = adapter.New(app)
 }
 
-func handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+// Handler is not exported
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	// If no name is provided in the HTTP request body, throw an error
 	return fiberLambda.ProxyWithContext(ctx, req)
 }
 
 func main() {
-	lambda.Start(handler)
+	//
+	lambda.Start(Handler)
 }
